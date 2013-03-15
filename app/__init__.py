7b17269cf44cabf2b7f612fa9881c3500b212cf4
Joe Hand <joe@joehand.org>
2013-03-14 18:21:17 -0600

    fix silly mistakes

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
@@ -9,13 +11,12 @@ from flask.ext.mongoengine import MongoEngine
 app.config['MONGODB_DB'] = app.config['DB_NAME'] or os.environ.get('DB_NAME')
 #For Heroku MongoHQ Add-On
 if os.environ.get('MONGOHQ_URL'):
-        connect(app.config['DB_NAME'], host=os.environ.get('MONGOHQ_URL'))
-    else:
-        connect(app.config['DB_NAME'], host='mongodb://' + app.config['DB_USERNAME'] + ':' + app.config['DB_PASSWORD'] + '@' + app.config['DB_HOST_ADDRESS'])
+    connect(app.config['DB_NAME'], host=os.environ.get('MONGOHQ_URL'))
+else:
+    connect(app.config['DB_NAME'], host='mongodb://' + app.config['DB_USERNAME'] + ':' + app.config['DB_PASSWORD'] + '@' + app.config['DB_HOST_ADDRESS'])
 db = MongoEngine(app)
 
 #login/openid
-import os
 from flask.ext.login import LoginManager
 from flask.ext.openid import OpenID
 from config import basedir
