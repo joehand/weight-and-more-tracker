64f08344b211aec0d582131bcc008dc3a00eb52c
Joe Hand <joe@joehand.org>
2013-03-17 17:05:04 -0600

    Add basic API

---
+++ app/api.py
@@ -0,0 +1,42 @@
+from app import api
+from models import User, Track
+
+from flask.ext.mongorest import MongoRest
+from flask.ext.mongorest.views import ResourceView
+from flask.ext.mongorest.resources import Resource
+from flask.ext.mongorest import operators as ops
+from flask.ext.mongorest.methods import *
+from flask.ext.mongorest.authentication import AuthenticationBase
+
+class SessionAuthentication(AuthenticationBase):
+    def authorized(self):
+        return current_user.is_authenticated()
+
+class BaseResourceView(ResourceView):
+    authentication_methods = [SessionAuthentication]
+
+class UserResource(Resource):
+    document = User
+    uri_prefix = "/user/"
+
+@api.register(name='user', url='/user/')
+class UserView(ResourceView):
+    resource = UserResource
+    methods = [Create, Update, Fetch, List]
+
+class TrackResource(Resource):
+    document = Track
+    related_resources = {
+        'author': UserResource
+    }
+    filters = {
+        'author_id': [ops.Exact],
+    }
+    rename_fields = {
+        'author': 'author_id',
+    }
+
+@api.register(name='track', url='/track/')
+class TrackView(ResourceView):
+    resource = TrackResource
+    methods = [Create, Update, Fetch, List]
