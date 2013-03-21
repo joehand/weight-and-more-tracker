671da44096ea04c683065757d969ae19646e6e57
Joe Hand <joe@joehand.org>
2013-03-21 07:49:30 -0600

    Fix undefined error.

--- app/static/js/app/main.js
+++ app/static/js/app/main.js
@@ -12,18 +12,17 @@ define(['backbone', 'underscore', 'jquery', 'model/userModel', 'model/postModel'
         });
     }
 
-    console.log('Starting main viz');
-    //Load the bootstrap models (generated in template)
-    var posts = new Posts(postsBootstrap);
-    var user = new User(userBootstrap);
+    // Make sure we have bootstrapped models
+    if (typeof postsBootstrap !== 'undefined') {
+        console.log('Starting main viz');
+        //Load the bootstrap models (generated in template)
+        var posts = new Posts(postsBootstrap) || null;
+        var user = new User(userBootstrap) || null;
+        //Start up the main view passing the posts as our collection and user as the model. boom.
 
-    posts.url = '/data/?author__exact=' + user.id;
-    posts.fetch({update:true, remove: false})
-
-    //Start up the main view passing the posts as our collection and user as the model. boom.
-
-    var vizView = new VizView({
-        collection : posts,
-        model : user
-    });
+        var vizView = new VizView({
+            collection : posts,
+            model : user
+        });  
+    }
 });
